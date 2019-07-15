e82b6cb2e6c5c5be8c4ed47a2215a91b0f5d5397
Adam T. Croft <sendtoatc@gmail.com>
2019-07-14 18:07:06 -0700

    setup vectorgraphic .h template

---
+++ Assignment1/VectorGraphic.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "VectorGraphic.h"
+
+
+VectorGraphic::VectorGraphic()
+{
+}
+
+
+VectorGraphic::~VectorGraphic()
+{
+}
---
+++ Assignment1/VectorGraphic.h
@@ -0,0 +1,26 @@
+#pragma once
+class VectorGraphic
+{
+public:
+	VectorGraphic();
+	~VectorGraphic();
+
+	void addPoint(const Point& p);
+	void removePoint(const Point& p);
+	void erasePoint(int index);
+
+	void openShape();
+	void closeShape();
+
+	bool isOpen() const;
+	bool isClosed() const;
+	int getWidth() const;
+	int getHeight() const;
+
+	int getPointCount() const;
+	Point getPoint(int index) const;
+
+private:
+	Points myPath;
+};
+
